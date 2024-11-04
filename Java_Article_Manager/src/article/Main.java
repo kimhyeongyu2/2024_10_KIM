1025bfe1e9069253681284b477989d795bdf7ffb
kimhyeongyu2 <127837817+kimhyeongyu2@users.noreply.github.com>
2024-11-04 11:37:32 +0900

    게시물 작성 기능 구현

---
+++ Java_Article_Manager/src/article/Main.java
@@ -0,0 +1,47 @@
+package article;
+
+import java.util.Scanner;
+
+public class Main {
+		public static void main(String[] args) {
+			System.out.println("==프로그램 시작==");
+
+			Scanner sc = new Scanner(System.in);
+			
+			int lastArticleId = 0;
+			
+			while(true) {
+				System.out.println("명령어");
+				String cmd = sc.nextLine();
+				System.out.println("입력된 명령어)" + cmd);
+				
+				if(cmd.equals("exit")) {
+					break;
+				} else if (cmd.equals("article list")) {
+					
+					System.out.println("게시글이 없습니다");
+					
+				}	else if (cmd.equals("article write")) {
+					lastArticleId++;
+					System.out.println("제목 : ");
+					String title = sc.nextLine();
+					System.out.println("내용 : ");
+					String body = sc.nextLine();
+					
+					System.out.printf("%d번글이 생성되었습니다\n",lastArticleId);
+					
+				} else {
+						System.out.println("존재하지 않는 명령어입니다");
+					}	
+					
+					
+					
+			}
+			
+			sc.close();
+			
+			System.out.println("==프로그램 끝==");
+
+		}
+		
+}
